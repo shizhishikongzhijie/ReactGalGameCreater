6507de9b9a6e45e22e37e24d8c7aa43823c31aac
zhang-ruoteng <3473901836@qq.com>
2024-04-01 21:22:28 +0800

    增加less

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # 一个网页版galgame制作器
+## 项目结构
+### tree
+D:.
+└─galgamecreater
+    ├─node_modules
+    ├─public
+    ├─server
+    │  ├─bin
+    │  ├─public
+    │  │  ├─images
+    │  │  ├─javascripts
+    │  │  └─stylesheets
+    │  ├─routes
+    │  └─views
+    └─src
--- galgamecreater/src/App.js
+++ galgamecreater/src/App.js
@@ -1,20 +1,41 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.less';
 import axios from 'axios';
-function file(){
+import { Layout, Flex } from 'antd';
+const { Header, Content, Footer, Sider } = Layout;
+function file() {
   axios.get('http://localhost:3300/file')
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
+var windowWidh,windowHeight;
 function App() {
+  //实时获得窗口大小
+  window.onresize = function () {
+    console.log(window.innerWidth, window.innerHeight);
+    windowWidh = window.innerWidth;
+    windowHeight = window.innerHeight;
+  }
   return (
-    <div className="App" >
-      file
-      {file()}
+
+    <div className="App" style={{width :"auto",height:"auto"}}>{file()}
+      <Layout className='layoutStyle'>
+        <Header className='headerStyle'>Header</Header>
+        <Layout>
+          <Layout>
+          <Content className='contentStyle'>Content</Content>
+          <Footer className='footerStyle'>Footer</Footer>
+          </Layout>
+          <Sider width="25%" className='siderStyle'>
+            Sider
+          </Sider>
+        </Layout>
+        
+      </Layout>
     </div>
   );
 }
